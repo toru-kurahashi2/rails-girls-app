1237de53e30a3e05b074482ba94c1ec1ff8acda5
j2121matsue <166687407+j2121matsue@users.noreply.github.com>
2024-12-14 07:52:05 +0000

    文言

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,4 @@
 # app/models/idea.rb
 class Search < ApplicationRecord
-  scope :search, ->(query) { where("description LIKE ? OR name LIKE ?", "%#{query}%", "%#{query}%") }
+  scope :search, ->(query) { where("description LIKE ? OR name LIKE ? OR user_name LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%") }
 end
